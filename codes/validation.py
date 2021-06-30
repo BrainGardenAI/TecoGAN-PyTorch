1eab247804685f81377c2a0b7edb6d0d383fad4a
georgepsh <georgii39@gmail.com>
2021-06-30 18:13:46 +0300

    debug

--- codes/validation.py
+++ codes/validation.py
@@ -73,6 +73,7 @@ def validate_gen(opt, model, logger, dataset_idx, model_idx):
 
                 filename = '{}/{}.jpg'.format(res_seq_dir, frm_idx)
                 res_img = np.stack([hr_frm, lr_frm], axis=1)
+                print(res_img.shape)
                 cv2.imwrite(filename, res_img)
 
 
